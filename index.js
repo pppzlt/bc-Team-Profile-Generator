65c1817fe026c29f8f396070d4829368dbe65dd0
Lantao home PC <lantaook@gmail.com>
2023-01-28 16:23:09 -0500

    will use call back hell recursive function but performance will be bad

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const { renderManger } = require("./src/renderManager");
 const { renderEngineer } = require("./src/renderEngineer");
 const { renderIntern } = require("./src/renderIntern");
 
-const prompt = inquirer.createPromptModule();
+const prompt = inquirer.prompt;
 
-fs.writeFileSync("./dist/index.html", "");
+// fs.writeFileSync("./dist/index.html", "");
 
 let manager = [];
 let engineer = [];
@@ -19,54 +19,46 @@ let p_1 = () => prompt([questions[1]]);
 let p_2 = () => prompt([questions[2]]);
 let p_3 = () => prompt([questions[3]]);
 
-prompt([questions[0], questions[1]])
-  .then((ans) => {
-    console.log(ans);
-    manager.push(ans.manager.split(" "));
-    console.log(manager);
-    //initialte a new manager instance
-    //assign option value so to run (while loop) change to recursive calls. otherwise have to use async/await
-    option = ans.options;
-    check_option(option);
-  })
-  .then(() => {
-     console.log(
-       "manage" +
-         manager.length +
-         "\nengineer" +
-         engineer.length +
-         "\nintern" +
-         intern.length
-     );
-  });
 
-// let manager1 = new Manager(...manager);
+prompt([questions[0], questions[1]]).then((ans) => {
+  manager.push(ans.manager.split(" "));
+  //assign option value so to run (while loop) change to recursive calls. otherwise have to use async/await
+  option = ans.options;
+  check_option(option);
+});
 
 //use function recursion to do loop
+// ----------------------------------callback hell. not a good coding manner. either async/await and while loop, or return new promise in then()
 function check_option(op) {
   if (op === "Add an engineer") {
     p_2().then((ans) => {
-      console.log(ans);
+      // console.log(ans);
       engineer.push(ans.engineer.split(" "));
       p_1().then((ans) => {
-        console.log(ans);
+        // console.log(ans);
         option = ans.options;
+        //recursive call
         check_option(option);
       });
     });
   } else if (op === "Add an intern") {
     p_3().then((ans) => {
-      console.log(ans);
+      // console.log(ans);
       intern.push(ans.intern.split(" "));
       p_1().then((ans) => {
-        console.log(ans);
+        // console.log(ans);
         option = ans.options;
+        //recursive call
         check_option(option);
       });
     });
   } else {
     //finish
-   
-    return;
+    // createComponents();
+    // createFile();
+    // appendComponents();
+    console.log(`${manager.length}  ${engineer.length}   ${intern.length}`)
   }
 }
+
+
