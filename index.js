6f5f24e59eac411d06f5f9f74992dfe21f3042d3
Lantao home PC <lantaook@gmail.com>
2023-01-29 15:30:54 -0500

    fixed callback hell, and this should be more readable! also used switch case instead of if

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { renderEngineer } = require("./src/renderEngineer");
 const { renderIntern } = require("./src/renderIntern");
 const { top, bottom } = require("./src/components");
 
-
 // store all user inputs below
 let manager = [];
 let engineer = [];
@@ -14,52 +13,95 @@ let intern = [];
 let option = "";
 
 const prompt = inquirer.prompt;
-// p_1 ask the options, p_2 if engineer, p_3 if intern
+// p_0 ask the manager, p_1 ask the options, p_2 if engineer, p_3 if intern
+let p_0 = () => prompt([questions[0]]);
 let p_1 = () => prompt([questions[1]]);
 let p_2 = () => prompt([questions[2]]);
 let p_3 = () => prompt([questions[3]]);
 
-prompt([questions[0], questions[1]]).then((ans) => {
-  manager.push(ans.manager.split(" "));
-  //assign option value so to run (while loop) change to recursive calls. otherwise have to use async/await
-  option = ans.options;
-  return check_option(option);
-});
+p_0()
+  .then((ans) => {
+    manager.push(ans.manager.split(" "));
+    return p_1();
+  })
+  .then((ans) => {
+    option = ans.options;
+    return check_option(option);
+  });
 
 //use function recursion to do loop
-// ----------------------------------callback hell. not a good coding manner. either async/await and while loop, or return new promise in then()
 function check_option(op) {
-  if (op === "Add an engineer") {
-    p_2().then((ans) => {
-      // console.log(ans);
-      engineer.push(ans.engineer.split(" "));
-      p_1().then((ans) => {
-        // console.log(ans);
-        option = ans.options;
-        //recursive call
-        return check_option(option);
-      });
-    });
-  } else if (op === "Add an intern") {
-    p_3().then((ans) => {
-      // console.log(ans);
-      intern.push(ans.intern.split(" "));
-      p_1().then((ans) => {
-        // console.log(ans);
-        option = ans.options;
-        //recursive call
-        return check_option(option);
-      });
-    });
-  } else {
-    //finish
-    console.log(`${manager.length},${engineer.length},${intern.length}`)
-    let cards = createComponents();
-    createFile();
-    appendComponents(cards);
-    return;
+  switch (op) {
+    case "Add an engineer":
+      p_2()
+        .then((ans) => {
+          engineer.push(ans.engineer.split(" "));
+          return p_1();
+        })
+        .then((ans) => {
+          option = ans.options;
+          //recursive call
+          return check_option(option);
+        });
+      break;
+    case "Add an intern":
+      p_3()
+        .then((ans) => {
+          intern.push(ans.intern.split(" "));
+          return p_1();
+        })
+        .then((ans) => {
+          option = ans.options;
+          //recursive call
+          return check_option(option);
+        });
+      break;
+    case "Finish building team":
+      //finish
+      console.log(`${manager.length},${engineer.length},${intern.length}`);
+      let cards = createComponents();
+      createFile();
+      appendComponents(cards);
+      return;
   }
 }
+//---------------------------------------using if statement
+// if (op === "Add an engineer") {
+//   p_2()
+//     .then((ans) => {
+//       // console.log(ans);
+//       engineer.push(ans.engineer.split(" "));
+//       return p_1();
+//     })
+//     .then((ans) => {
+//       // console.log(ans);
+//       option = ans.options;
+//       //recursive call
+//       return check_option(option);
+//     });
+// } else if (op === "Add an intern") {
+//   p_3()
+//     .then((ans) => {
+//       // console.log(ans);
+//       intern.push(ans.intern.split(" "));
+//       return p_1();
+//     })
+//     .then((ans) => {
+//       // console.log(ans);
+//       option = ans.options;
+//       //recursive call
+//       return check_option(option);
+//     });
+// } else {
+//   //finish
+//   console.log(`${manager.length},${engineer.length},${intern.length}`);
+//   let cards = createComponents();
+//   createFile();
+//   appendComponents(cards);
+//   return;
+// }
+// }
+
 // create cards components
 function createComponents() {
   let components = "";
@@ -84,8 +126,7 @@ function appendComponents(content) {
   fs.writeFileSync("./dist/index.html", bottom, { flag: "a" });
 }
 
-
-//----------------------------------------------using async await and while loop, should perform better! worked, but has to remove all those then(). leave and comment out below.
+//----------------------------------------------using async await and while loop, should perform better? maybe? (maybe not asyn/await just syntax sugar) also worked, but has to remove all those then(). leave and comment out below.
 // (async () => {
 //   prompt([questions[0], questions[1]]).then(async (ans) => {
 //     console.log(ans);
